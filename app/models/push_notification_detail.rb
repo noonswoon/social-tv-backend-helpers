d73db4b73e62288248d1e6a927552b079f3c9b9a
Kavin Mickey Asavanant <akavin@gmail.com>
2012-07-16 12:20:21 +0700

    try to revert stuff back

--- app/models/push_notification_detail.rb
+++ app/models/push_notification_detail.rb
@@ -2,5 +2,5 @@ class PushNotificationDetail < ActiveRecord::Base
   attr_accessible :notify_when_comment, :notify_when_friend_checkin, :pn_device_token, :userid, :username
   validates :userid, :presence => true
   validates :username, :presence => true
-  validates :pn_device_token #, :presence => true
+  validates :pn_device_token, :presence => true
 end
