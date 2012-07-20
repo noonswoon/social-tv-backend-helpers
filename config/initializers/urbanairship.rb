cbac40b37a02ae4bf1955b4c7baa1d3d9e6ab11b
Kavin Mickey Asavanant <akavin@gmail.com>
2012-07-21 00:51:05 +0700

    change urbanairship key to the real ones

--- config/initializers/urbanairship.rb
+++ config/initializers/urbanairship.rb
@@ -1,6 +1,14 @@
-Urbanairship.application_key = 'Ip9pwoBrShK4zKfJ9RwC7w'
-Urbanairship.application_secret = 'qkswgsM0Qymqe0OIZX4ruw'
-Urbanairship.master_secret = 'SRu56K1YRVeoulsgvTmoUA'
+#development
+Urbanairship.application_key = '3NMTRP4CR6OvgcxCDJ8jBQ'
+Urbanairship.application_secret = 'lzEu_xY6QnyndSdQjUOXgg'
+Urbanairship.master_secret = '9RdeNn-OSUK7dCyipNnCFA'
+
+#production
+#Urbanairship.application_key = 'kzE6lIvvRkeyLlv4xOXAlQ'
+#Urbanairship.application_secret = '3JoZdd0HQQ2PH2m_wVZWGA'
+#Urbanairship.master_secret = 'BcjXSdFURZSDNCJh-lmfBQ'
+
 Urbanairship.logger = Rails.logger
 Urbanairship.request_timeout = 5 # default
+
 
