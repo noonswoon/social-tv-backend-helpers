a81f2925c96033b0ca5619d7f03c8ac12a59c6c0
Kavin Mickey Asavanant <akavin@gmail.com>
2012-07-26 22:41:00 +0700

    change from development to production for urbanairship

--- config/initializers/urbanairship.rb
+++ config/initializers/urbanairship.rb
@@ -1,12 +1,12 @@
 #development
-Urbanairship.application_key = '3NMTRP4CR6OvgcxCDJ8jBQ'
-Urbanairship.application_secret = 'lzEu_xY6QnyndSdQjUOXgg'
-Urbanairship.master_secret = '9RdeNn-OSUK7dCyipNnCFA'
+#Urbanairship.application_key = '3NMTRP4CR6OvgcxCDJ8jBQ'
+#Urbanairship.application_secret = 'lzEu_xY6QnyndSdQjUOXgg'
+#Urbanairship.master_secret = '9RdeNn-OSUK7dCyipNnCFA'
 
 #production
-#Urbanairship.application_key = 'kzE6lIvvRkeyLlv4xOXAlQ'
-#Urbanairship.application_secret = '3JoZdd0HQQ2PH2m_wVZWGA'
-#Urbanairship.master_secret = 'BcjXSdFURZSDNCJh-lmfBQ'
+Urbanairship.application_key = 'kzE6lIvvRkeyLlv4xOXAlQ'
+Urbanairship.application_secret = '3JoZdd0HQQ2PH2m_wVZWGA'
+Urbanairship.master_secret = 'BcjXSdFURZSDNCJh-lmfBQ'
 
 Urbanairship.logger = Rails.logger
 Urbanairship.request_timeout = 5 # default
